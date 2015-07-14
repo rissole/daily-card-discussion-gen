85f7d80a28ba9b16faf81ea1b2d7cb48a71a5e17
rissole <bonzaibrother@gmail.com>
2015-07-14 22:00:50 +1000

    wording

--- gen.js
+++ gen.js
@@ -3,3 +3,6 @@ var INTRO = '**Introduction**\n\n'
             + 'Cards selected will be random cards in the game until we literally go through every card *(Neutrals, class, spells, secrets, creatures, tokens*)\n\n'
             + 'At the bottom of each thread you can navigate to the previous card to continue it\'s discussion there.\n\n'
             + 'Because this is a community idea, please leave any suggestions down below to improve this thread.';
+
+var URL = "https://www.reddit.com/r/hearthstone/comments/3d2cqf/daily_card_discussion_thread_001_molten_giant/"
+var API = "https://www.mashape.com/omgvamp/hearthstone"
