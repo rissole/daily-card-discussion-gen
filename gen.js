b900c26cff0e41d35f06c75f95eb288cae41ecc6
rissole <bonzaibrother@gmail.com>
2015-07-22 09:13:59 +1000

    it's -> its

--- gen.js
+++ gen.js
@@ -1,7 +1,7 @@
 var INTRO = '**Introduction**\n\n'
             + 'Hello and welcome to the "Daily Card Discussion Thread", because people upvoted this suggestion a lot, I thought I could come a long and manage it and if people like it I will continue to post daily.\n\n'
             + 'Cards selected will be random cards in the game until we literally go through every card *(Neutrals, class, spells, secrets, creatures, tokens)*\n\n'
-            + 'At the bottom of each thread you can navigate to the previous card to continue it\'s discussion there.\n\n'
+            + 'At the bottom of each thread you can navigate to the previous card to continue its discussion there.\n\n'
             + 'Because this is a community idea, please leave any suggestions down below to improve this thread.';
 
 // should be fine we all have fast internet around here
